14abfe222701be9981f8e56e55e3ed97601dd9d4
Andrew Power <AndrewPwrP2P@gmail.com>
2018-04-07 14:56:08 -0700

    Update parity.c

--- parity.c
+++ parity.c
@@ -1,24 +1,28 @@
 //Project 1 07/04/18
 #include <stdio.h>
 
-main(){
-	int i,m,n, t;
+main (){
+	int i, m, n, t;
 	
 	i = 0;
 	m = 100;
 	n = 0;
 
-	while(i < m){
+	while (i < m){
+		
 		n++;
 		t = n;
-		while(t > 0){
+		
+		while (t > 0){
+			
 			t = t - 2;
+			
 			if (t == 0){
-				printf("%d is of even parity\n", n);
+				printf ("%d is of even parity\n", n);
 				break;
 			}else
 			if (t == 1){
-				printf("%d is of odd parity\n", n);
+				printf ("%d is of odd parity\n", n);
 				break;
 			}
 		} 	
